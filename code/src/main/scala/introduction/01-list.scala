5c3df310285427c59a7f94f4aa5632be8909ffa5
Hugh <hugh@localhost.localdomain>
2024-09-09 21:24:18 +0100

    Add implementation for session 1

--- code/src/main/scala/introduction/01-list.scala
+++ code/src/main/scala/introduction/01-list.scala
@@ -11,33 +11,33 @@ object StreamAsList {
   // Construction. We focus on creating Streams
 
   // The empty stream produces no values
-  val empty: Stream[Pure, Int] = ???
+  val empty: Stream[Pure, Int] = Stream.empty
 
   // This Stream should produce 1, 2, and 3, in that order
-  val naturals: Stream[Pure, Int] = ???
+  val naturals: Stream[Pure, Int] = Stream(1, 2, 3)
 
   // This method accepts a single value and returns the Stream that produces
   // that single value
-  def one[A](value: A): Stream[Pure, A] = ???
+  def one[A](value: A): Stream[Pure, A] = Stream(value)
 
   // This method accepts a list of values and returns the Stream that produces
   // exactly those values in the order given.
-  def list[A](values: List[A]): Stream[Pure, A] = ???
+  def list[A](values: List[A]): Stream[Pure, A] = Stream(values: _*)
 
   // Transformation. We focus on transforming existing Streams
 
   // This method returns a stream where every element in `stream` is incremented
   // by `value`.
-  def add(stream: Stream[Pure, Int], value: Int): Stream[Pure, Int] = ???
+  def add(stream: Stream[Pure, Int], value: Int): Stream[Pure, Int] = stream.map(_ + value)
 
-  // This methods return a stream that only contains the values of the input
+  // This method return a stream that only contains the values of the input
   // stream that match the predicate.
   def only[A](
       stream: Stream[Pure, A],
       predicate: A => Boolean
-  ): Stream[Pure, A] = ???
+  ): Stream[Pure, A] = stream.filter(predicate)
 
   // This method should sum all the values in the given `stream` and return a
   // Stream containing just a single value, the total.
-  def sum(stream: Stream[Pure, Int]): Stream[Pure, Int] = ???
+  def sum(stream: Stream[Pure, Int]): Stream[Pure, Int] = stream.fold1(_ + _)
 }
--- code/src/main/scala/introduction/02-kahan.scala
+++ code/src/main/scala/introduction/02-kahan.scala
@@ -3,9 +3,24 @@ package introduction
 import fs2.*
 
 object Kahan {
+
+  private case class CompensatedFloat(magnitude: Float, compensation: Float)
+  private object CompensatedFloat {
+    val zero = CompensatedFloat(0f, 0f)
+  }
+
+  private def add(total: CompensatedFloat, toAdd: Float): CompensatedFloat = {
+    val y: Float = toAdd - total.compensation
+    val t: Float = total.magnitude + y
+    CompensatedFloat(magnitude = t, compensation = (t - total.magnitude) - y)
+  }
+
   // Perform Kahan summation, returning a Stream with a single element that is
   // the total
-  def sum(stream: Stream[Pure, Float]): Stream[Pure, Float] = ???
+  def sum(stream: Stream[Pure, Float]): Stream[Pure, Float] =
+    stream
+      .fold(CompensatedFloat.zero)(add)
+      .map(_.magnitude)
 
   // Perform Kahan summation, returning a Stream where each element is the sum
   // of elements in the input up to that point.
@@ -13,5 +28,9 @@ object Kahan {
   // This is often more useful in a streaming application because the input may
   // never end, or we may want the most up-to-date result at any given point in
   // time.
-  def cumulativeSum(stream: Stream[Pure, Float]): Stream[Pure, Float] = ???
+  def cumulativeSum(stream: Stream[Pure, Float]): Stream[Pure, Float] =
+    stream
+      .scan(CompensatedFloat.zero)(add)
+      .map(_.magnitude)
+      .drop(1)
 }
